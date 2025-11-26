ccb5bc6c11596dcbc947ae3fa01dacb9af8ab0f4
tallestgabs <gabrielcastrodias1414@gmail.com>
2025-11-26 11:01:41 -0300

    implements SYB to optimizer.hs

--- trabalho3/LF2/Optimizer.hs
+++ trabalho3/LF2/Optimizer.hs
@@ -2,104 +2,37 @@ module Optimizer where
 
 import AbsLF
 import Interpreter
+import Data.Generics -- SYB
 
 optimizeP :: Program -> Program
-optimizeP (Prog fs) = Prog (map optimizeF fs)
+optimizeP = everywhere (mkT optimizeE)
        
-optimizeF :: Function -> Function
-optimizeF (Fun tR id decls exp) = Fun tR id decls (optimizeE exp)
-
 optimizeE :: Exp -> Exp
 optimizeE exp  = case exp of
-                      EStr str -> EStr str
-                      ETrue    -> ETrue
-                      EFalse   -> EFalse
-                      EInt n   -> EInt n
-                      EVar id  -> EVar id
-                      ENot exp -> let optExp  = optimizeE  exp
-                                      optENot = ENot optExp in
-                                      if (isLiteral optExp) 
-                                        then  wrapValueExpression (fst(eval [] [] optENot))
-                                        else  optENot
-                      ECon exp0 exp -> let optExp0 = optimizeE  exp0 
-                                           optExp  = optimizeE  exp   
-                                           optECon = ECon optExp0 optExp in 
-                                           if ((isLiteral optExp0) && (isLiteral optExp))
-                                             then wrapValueExpression (fst(eval [] [] optECon))
-                                             else optECon
-                      EAdd exp0 exp -> let optExp0 = optimizeE exp0
-                                           optExp  = optimizeE exp  
-                                           optEAdd = EAdd optExp0 optExp in 
-                                             if ((isLiteral optExp0) && (isLiteral optExp)) 
-                                               then wrapValueExpression (fst(eval [] [] optEAdd))
-                                               else optEAdd
--- TODO: substitua "undefined" abaixo pela otimização correspondente ao tipo de expressão.
--- @dica: estude a implementação fornecida da otimização das expressões anteriores
-                      ESub exp0 exp ->  let optExp0 = optimizeE exp0
-                                            optExp = optimizeE exp
-                                            optESub = ESub optExp0 optExp in
-                                                if((isLiteral optExp0) && (isLiteral optExp))
-                                                  then wrapValueExpression (fst(eval [] [] optESub))
-                                                  else optESub
 
-                      EMul exp0 exp ->  let optExp0 = optimizeE exp0 
-                                            optExp = optimizeE exp 
-                                            optEMul = EMul optExp0 optExp in 
-                                              if((isLiteral optExp0) && (isLiteral optExp))
-                                                then wrapValueExpression (fst(eval [] [] optEMul))
-                                                else optEMul 
+                      EAdd EInt(n1) EInt(n2) -> EInt(n1 + n2) 
+                      ESub EInt(n1) EInt(n2) -> EInt(n1 - n2)
+                      EMul EInt(n1) EInt(n2) -> EInt(n1 * n2)                       
+                      EDiv EInt(n1) EInt(n2) 
+                        | n2 /= 0 -> EInt(n1 `div` n2)
 
-                      EDiv exp0 exp -> let  optExp0 = optimizeE exp0 
-                                            optExp = optimizeE exp 
-                                            optEDiv = EDiv optExp0 optExp in 
-                                              if((isLiteral optExp0) && (isLiteral optExp))
-                                                then wrapValueExpression (fst(eval [] [] optEDiv))
-                                                else optEDiv
+                      EOr (ETrue) _          -> ETrue 
+                      EOr _ (ETrue)          -> ETrue 
+                      EOr (EFalse) (EFalse)  -> EFalse
 
-                      EOr  exp0 exp -> let  optExp0 = optimizeE exp0 
-                                            optExp = optimizeE exp 
-                                            optEOr = EOr optExp0 optExp in 
-                                              if((isLiteral optExp0) && (isLiteral optExp))
-                                                then wrapValueExpression (fst(eval [] [] optEOr))
-                                                else optEOr 
+                      EAnd (ETrue) (ETrue)   -> ETrue 
+                      EAnd (EFalse) _        -> EFalse
+                      EAnd _ (EFalse)        -> EFalse
 
-                      EAnd exp0 exp -> let  optExp0 = optimizeE exp0 
-                                            optExp = optimizeE exp 
-                                            optEAnd = EAnd optExp0 optExp in 
-                                              if((isLiteral optExp0) && (isLiteral optExp))
-                                                then wrapValueExpression (fst(eval [] [] optEAnd))
-                                                else optEAnd 
--- TODO: saiba explicar o motivo da otimização abaixo 					  
-                      ECall id lexp   -> ECall id (map (\expr ->  optimizeE expr) lexp)
-                      -- vai car no pattern matching do ECall e vai desmontar o identificador e a lista de expressao
-                      -- o retorno continuara sendo uma chamada com o mesmo nome, porem cada elemento da lista sera otimizada atraves do optimizeE, o que no fim retornara 
-                      -- a lista de parametros otimizada
-                       
--- TODO: crie um programa exemplo em que a otimização abaixo seja usada
-                      EIf exp expT expE -> let optExp  = optimizeE exp 
-                                               optThen = optimizeE expT
-                                               optElse = optimizeE expE 
-                                               optEIf  = EIf optExp optThen optElse in 
-                                                 case optExp of
-                                                   EInt vExpIf -> if (vExpIf == 0)
-                                                                     then optElse
-                                                                     else optThen
-                                                   _              -> optEIf
+                      ENot (ETrue)           -> EFalse 
+                      ENot (EFalse)          -> ETrue
 
-                        
-isLiteral :: Exp -> Bool
-isLiteral exp = case exp of
-                        EStr  _        -> True
-                        ETrue          -> True
-                        EFalse         -> True
-                        EInt  _        -> True
-                        _              -> False
+                      ECon EStr(s1) EStr(s2) -> EStr(s1 ++ s2)
+                      EIf EInt(n) expT expE
+                        | n /= 0             -> expT                          
+                        | otherwise          -> expE
+                      
+                      EIf (ETrue) expT _     -> expT
+                      EIf (EFalse) _ expE    -> expE
 
-wrapValueExpression :: Valor -> Exp 
-wrapValueExpression (ValorInt i) = EInt i
-wrapValueExpression (ValorStr s) = EStr s
-wrapValueExpression (ValorBool True) = ETrue
-wrapValueExpression (ValorBool False) = EFalse
-
-
-
+                      _ -> exp 
