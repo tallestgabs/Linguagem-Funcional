6f013f53c4681d4e054b83792ba08096999ca87c
tallestgabs <gabrielcastrodias1414@gmail.com>
2025-12-03 11:25:39 -0300

    add logs for optimizer with monads

--- trabalho3/LF2/Optimizer.hs
+++ trabalho3/LF2/Optimizer.hs
@@ -1,24 +1,50 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Optimizer where
 
 import AbsLF
 import Interpreter
 import Data.Generics -- SYB
+import Control.Monad.Writer
+import Control.Monad.State
 
-optimizeP :: Program -> Program
-optimizeP = everywhere (mkT optimizeE)
+type OptM a = Writer [String] a
+
+optimizeP :: Program -> (Program, [String])
+optimizeP p = runWriter (everywhereM (mkM optimizeEM) p)
        
-optimizeE :: Exp -> Exp
-optimizeE exp  = case exp of
-                      EIf (EInt n) expT expE
-                        | n /= 0             -> expT                          
-                        | otherwise          -> expE
-                      
-                      EIf (ETrue) expT _     -> expT
-                      EIf (EFalse) _ expE    -> expE
+optimizeEM :: Exp -> OptM Exp
+optimizeEM exp  = case exp of
+                      EIf (EInt n) expT expE ->
+                          if n /= 0
+                          then do
+                              tell ["Optimizing IF for THEN"]                       
+                              return expT
 
-                      _ -> if(isGround exp)
-                              then wrapValueExpression  (fst (eval [] [] exp))
-                              else exp
+                          else do 
+                              tell ["Optimizing IF for ELSE"]
+                              return expE
+
+                      
+                      EIf (ETrue) expT _     -> do
+                          tell ["Optimizing IF (TRUE)"]
+                          return expT
+
+                      EIf (EFalse) _ expE    -> do
+                          tell ["Optimizing IF (FALSE)"] 
+                          return expE
+
+                      _ -> if isGround exp
+                              then do 
+                                  let (val, _) = eval [] [] exp 
+                                  let newValue = wrapValueExpression val
+
+                                  if exp /= newValue
+                                     then tell ["Constant Folding: " ++ show exp ++ " -> " ++ show newValue]
+                                     else return () -- se ja era literal nao faz nada
+                                  
+                                  return newValue 
+
+                              else return exp
 
                         
 isGround:: Exp -> Bool 
